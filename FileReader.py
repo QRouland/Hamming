91561de8884c280a5fe1d7de84452a2618b6b461
danfr <clement.arnaudeau@yahoo.fr>
2016-01-14 17:59:17 +0100

    Documentation

--- FileReader.py
+++ FileReader.py
@@ -1,5 +1,11 @@
 class FileReader:
     def __init__(self, filepath):
+        """
+        Constructeur de la classe, lit le fichier entre en parametre,
+        le rammene a une longueur multiple de 4 et place son contenu dans l'attibut data
+        :param filepath: chemin du fichier source
+        :return: contenu du fichier formatte dans data
+        """
         with open(filepath, 'r') as myfile:
             self.data = myfile.read().replace('\n', '').replace('\r', '')
 
--- NoiseGenerator.py
+++ NoiseGenerator.py
@@ -4,6 +4,12 @@ import random
 class NoiseGenerator:
     @staticmethod
     def bool_random(proba):
+        """
+        Genere un booleen aleatoire suivant la probabilite entree en parametre,
+        plus elle sera grande, plus le resultat aura de chances d'etre True
+        :param proba: Probablilite d'obtenir True (en %)
+        :return: True ou False
+        """
         coefs = {True : proba, False: 100-proba}
         number = random.random() * sum(coefs.values())
         for k,v in coefs.items():
@@ -14,6 +20,11 @@ class NoiseGenerator:
 
     @staticmethod
     def convert(string):
+        """
+        Ajoute du bruit dans une chaine de 0 ou 1
+        :param string: chaine a convertir
+        :return: chaine convertie
+        """
         s = list(string)
         i = 0
         while i < len(s):
