74482e2ffcba4db36d6872a06019db0e00efd289
Quentin Rouland <quentin.roualnd.82@gmail.com>
2016-01-19 15:34:08 +0100

    Hamming Decode & correction

--- Hamming.py
+++ Hamming.py
@@ -2,18 +2,6 @@ import sys
 
 
 class Hamming:
-    def encode(self, message):
-        """
-        Encode le message dans un fichier emis.txt
-        :param message: Le message a encoder
-        :return:
-        """
-        with open("emis.txt", "w") as f:
-            while len(message) >= 4:
-                mot = message[0:4]
-                f.write(self.hamming(mot) + "\n")
-                message = message[4:]
-
     def hamming(self, mot):
         """
         Retourne le mot de code (mot 4bits + les bits de parite)
@@ -22,9 +10,10 @@ class Hamming:
         """
         return "".join(
                 [mot,
-                 self.parite(mot, [0, 1, 2]),
                  self.parite(mot, [1, 2, 3]),
-                 self.parite(mot, [0, 2, 3])])
+                 self.parite(mot, [0, 2, 3]),
+                 self.parite(mot, [0, 1, 3]),
+                 ])
 
     def parite(self, mot, indices):
         """
@@ -33,13 +22,38 @@ class Hamming:
         :param indices: Indice des bits ou effectuer la parite
         :return: parite pour les bits correspondants aux indices du mot
         """
-        """Compute the parity bit for the given string s and indicies"""
         return str(sum([int(mot[i]) for i in indices if mot[i] == "1"]) % 2)
 
 
-# Main
+    def correction(self, mot):
+        """
+        Corrige le mot avec hamming
+        :param mot: mot a corriger
+        :return: mot corrige
+        """
+        # Calculate syndrome
+        s = [
+            (int(mot[1]) + int(mot[2]) + int(mot[3]) + int(mot[4])) % 2,
+            (int(mot[0]) + int(mot[2]) + int(mot[3]) + int(mot[5])) % 2,
+            (int(mot[0]) + int(mot[1]) + int(mot[3]) + int(mot[6])) % 2,
+        ]
 
-if __name__ == "__main__":
-    input_string = input("LOL :")
-    hamming = Hamming()
-    hamming.encode(input_string)
+        b = 4 * s[2] + 2 * s[1] + s[0]
+
+        if b not in [0]:
+            SYNDROME_CHECK = [-1, 4, 5, 2, 6, 1, 0, 3]
+            y = list(mot)
+            y[SYNDROME_CHECK[b]] = str((int(y[SYNDROME_CHECK[b]]) + 1) % 2)
+            mot = "".join(y)
+        return mot
+
+    def decode(self, mot):
+        """
+        Decode le mot
+        :param mot: mot a decoder
+        :return: mot decode
+        """
+        return mot[0:4]
+
+
+
