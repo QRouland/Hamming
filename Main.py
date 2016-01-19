824e40afd364c3f8ad44a9727f08f89c75f8101c
danfr <clement.arnaudeau@yahoo.fr>
2016-01-19 15:18:24 +0100

    Main terminé

--- Main.py
+++ Main.py
@@ -17,3 +17,19 @@ FileUtil.write("emis.txt", emis)
 recu = NoiseGenerator.convert(emis)
 
 FileUtil.write("recu.txt", recu)
+correct = ""
+
+while len(recu) >= 7:
+    mot = recu[0:7]
+    correct += ham.correction(mot)
+    recu = recu[7:]
+
+FileUtil.write("correct.txt", correct)
+message = ""
+
+while len(correct) >= 7:
+    mot = recu[0:7]
+    message += ham.decode(mot)
+    correct = correct[7:]
+
+FileUtil.write("message.txt", message)
